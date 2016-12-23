4d0bb89f19613b2ccfe6f8e2aee80383c2824763
charlesDataCenterFPGA <chenqian21@me.com>
2016-12-23 14:31:13 +0800

    Update FpgaLibraryLoader.java

--- FPGAbasedHadoopDecompressionAcceleration/src/main/java/com/hadoop/compression/lzo/FpgaLibraryLoader.java
+++ FPGAbasedHadoopDecompressionAcceleration/src/main/java/com/hadoop/compression/lzo/FpgaLibraryLoader.java
@@ -1,26 +1,31 @@
-package com.hadoop.compression.lzo;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 /**
  * Created by Chen Qian on 1/20/2016.
  * intel Corp.
  */
  
+package com.hadoop.compression.lzo;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
 public class FpgaLibraryLoader {
 
     private static final Log LOG = LogFactory.getLog(FpgaLibraryLoader.class);
     private static boolean fpgaLibraryLoaded = false;
-
     static {
         try {
             //try to load the lib
-            System.loadLibrary("fpgadecompress");
+            System.out.println("com.hadoop.compression.lzo.FpgaLibraryLoader: loader ~~");
+            System.loadLibrary("FpgaDecompressor");
+	    //System.loadLibrary("NativeValidation");
+            
             fpgaLibraryLoaded = true;
-            LOG.info("Loaded fpga decompress library");
+            System.out.println("com.hadoop.compression.lzo.FpgaLibraryLoader: loader success");
+            LOG.info("com.hadoop.compression.lzo.FpgaLibraryLoader: Loaded fpga decompress library");
         } catch (Throwable t) {
-            LOG.error("Could not load fpga decompress library", t);
+            System.out.println("com.hadoop.compression.lzo.FpgaLibraryLoader: loader failed");
+            LOG.error("com.hadoop.compression.lzo.FpgaLibraryLoader: Could not load fpga decompress library", t);
             fpgaLibraryLoaded = false;
         }
     }
